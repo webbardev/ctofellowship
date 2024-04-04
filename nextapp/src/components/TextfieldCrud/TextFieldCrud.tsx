c24d1fc1988139ef49b0f2a9cac5ac0059a46b7a
Adrian Stanek <stanek@bitsinmotion.de>
2024-04-04 14:57:46 +0200

    Implement input validation for TextFieldCrud component
    This commit enhances the TextFieldCrud component by adding input validation. It only allows characters and numbers [a-zA-Z0-9] inside the textfield. New tests have also been added to incorporate the validation logic and verify that it functions as expected.

--- nextapp/src/components/TextfieldCrud/TextFieldCrud.tsx
+++ nextapp/src/components/TextfieldCrud/TextFieldCrud.tsx
@@ -1,22 +1,45 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import '../../../styles/globals.scss';
 
 interface ITextFieldCrud {}
 
 export const TextFieldCrud: React.FC<ITextFieldCrud> = () => {
     const [value, setValue] = useState('');
+    const [showValidation, setShowValidation] = useState(false);
+
+    const [isValid, setIsValid] = useState<boolean>(true);
 
     const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        // Check if input is valid
+        const regex = /^[a-zA-Z0-9]*$/;
+
+        if (!regex.test(event.target.value)) {
+            setIsValid(false);
+            setShowValidation(true);
+        } else {
+            setIsValid(true);
+            setShowValidation(false);
+        }
+
         setValue(event.target.value);
     }, []);
 
     const handleSubmit = useCallback(() => {}, []);
 
+    const validationStyles = useMemo(() => {
+        if (showValidation && !isValid) {
+            return 'border-red-500 text-red-500';
+        }
+
+        return 'border-neutral-700 text-neutral-700';
+    }, [isValid, showValidation]);
+
     return (
         <section className="relative grid w-full grid-cols-2 gap-2">
             <input
-                className="rounded border border-neutral-700 p-2"
+                className={`rounded border p-2 ${validationStyles}`}
                 data-test-id="textfieldcrud-input"
+                data-test-validation={isValid ? '1' : '0'}
                 value={value}
                 onChange={handleChange}
             />
--- nextapp/src/components/TextfieldCrud/tests/TextFieldCrud.cy.tsx
+++ nextapp/src/components/TextfieldCrud/tests/TextFieldCrud.cy.tsx
@@ -19,4 +19,25 @@ describe('<TextFieldCrud />', () => {
             .should('not.be.disabled')
             .and('be.visible');
     });
+
+    it('Textfield only accepts [a-zA-Z0-9], validation will be turned on', () => {
+        cy.mount(<TextFieldCrud />);
+
+        cy.get('[data-test-id="textfieldcrud-input"]', { timeout: 500 })
+            .clear()
+            .type('Hello World!@#$%^&*()_+');
+
+        // 🔴 Check if text was correctly sanitized
+        cy.get('[data-test-id="textfieldcrud-input"][data-test-validation="0"]', {
+            timeout: 500,
+        }).should('have.class', 'border-red-500 text-red-500');
+
+        cy.get('[data-test-id="textfieldcrud-input"]', { timeout: 500 })
+            .clear()
+            .type('HelloWorld007');
+
+        cy.get('[data-test-id="textfieldcrud-input"][data-test-validation="1"]', {
+            timeout: 500,
+        }).should('not.have.class', 'border-red-500 text-red-500');
+    });
 });
